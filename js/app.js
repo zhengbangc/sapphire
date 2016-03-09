f3d12b5c28a9c181d701233a896653dee8a0ad94
zhengbangc <zhengbangc@gmail.com>
2016-03-08 21:13:27 -0600

    ci

---
+++ js/app.js
@@ -0,0 +1,1 @@
+var app=angular.module("mp3",["ngRoute"]);app.config(function($routeProvider){$routeProvider.when("/gallery",{templateUrl:"partials/gallery.html",controller:"galleryController"}).when("/list",{templateUrl:"partials/list.html",controller:"listController"}).when("/details/:rank",{templateUrl:"partials/details.html",controller:"detailsController"}).otherwise({redirectTo:"/list"})});
